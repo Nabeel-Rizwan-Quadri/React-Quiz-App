bd7f418e6ddf317d0badf632584a864285756196
Nabee-Rizwan-Quadri <80901728+Nabee-Rizwan-Quadri@users.noreply.github.com>
2021-10-02 08:18:43 +0500

    Update App.js
    variables changed questions added

--- src/App.js
+++ src/App.js
@@ -1,53 +1,131 @@
 import { useState } from "react"
+import './index.js'
 import './App.css'
 
-
 function App() {
+    
+    // var Progress = require('react-progressbar');
 
   const questions = [
     {
-      Question: "what is JSX",
+      Question: "What is JSX?",
       option: [
         "JavaScript XML",
         "JawaScript XML",
         "JavaScript",
         "JS"
       ],
-      correctAnswer: "JavaScript XML",
-      isCorrect: false
+      correctAnswer: "JavaScript XML"
     }, 
     {
-      Question: "what is CSS",
+      Question: "What is CSS",
       option: [
         "Call Style Sheet",
         "Calling Styles Shirt",
         "Cascading Style Sheet",
         "CSS"
       ],
-      correctAnswer: "Cascading Style Sheet",
-      isCorrect: false
+      correctAnswer: "Cascading Style Sheet"
     },
     {
-      Question: "what is JS",
+      Question: "What is JS?",
       option: [
         "JavaScript",
         "XML",
         "CSS",
         "JS"
       ],
-      correctAnswer: "JavaScript",
-      isCorrect: false
+      correctAnswer: "JavaScript"
     },
     {
-      Question: "what is HTML",
+      Question: "What is SQL?",
       option: [
-        "Hypertext Makeup Language",
-        "Hypertext Markup Loan",
-        "Hypertxt Markup Language",
-        "Hypertext Markup Language"
+        "Structure Querry Language",
+        "Structure Quick Language",
+        "SQL",
+        "Strict Querry Lan"
       ],
-      correctAnswer: "Hypertext Markup Language",
-      isCorrect: false
+      correctAnswer: "Structure Querry Language"
+    },
+    {
+      Question: "Which is an input device?",
+      option: [
+        "Mice",
+        "Monitor",
+        "Speaker",
+        "Mouse"
+      ],
+      correctAnswer: "Mouse"
+    },
+    {
+      Question: "Which is output device?",
+      option: [
+        "Mouse",
+        "Monitor",
+        "Keyboard",
+        "Trackpad"
+      ],
+      correctAnswer: "Monitor"
+    },
+    {
+      Question: "Which is the most powerful computer?",
+      option: [
+        "Mini computer",
+        "Super computer",
+        "Mainframe computer",
+        "Wokstation computer"
+      ],
+      correctAnswer: "Super computer"
+    },
+    {
+      Question: "What is CISCO?",
+      option: [
+        "Programing Language",
+        "Computer",
+        "Networking software",
+        "Hacking software"
+      ],
+      correctAnswer: "Networking software"
+    },
+    {
+      Question: "What is Linux?",
+      option: [
+        "Networking software",
+        "Operating system",
+        "Hacking software",
+        "Operating software"
+      ],
+      correctAnswer: "Operating system"
+    },
+    {
+      Question: "What is PSU?",
+      option: [
+        "Power Supply Unit",
+        "Power Suppliment Unit",
+        "Power Suppliment UI",
+        "Power Supply UI"
+      ],
+      correctAnswer: "Power Supply Unit"
+    },
+    {
+      Question: "What is GPU?",
+      option: [
+        "Grade Percentage Unit",
+        "Video card",
+        "Graphics Processing Unit",
+        "CPU"
+      ],
+      correctAnswer: "Graphics Processing Unit"
+    },
+    {
+      Question: "How was this Quiz?",
+      option: [
+        "Easy",
+        "Very Easy",
+        "Very Very Easy",
+        "Very Very Very Easy"
+      ],
+      correctAnswer: "Very Easy"
     }
   ]
 
@@ -58,18 +136,18 @@ function App() {
     //To cycle the questions
     setcurrentQuestionIndex(currentQuestionIndex + 1)
 
-    //to cross check the selected radio button with correct answer
-    const rbs = document.querySelectorAll('input[name="choice"]');
+    //to cross check which radio button was selected and saving its value for right or wrong option
+    const options = document.querySelectorAll('input[name="choice"]');
     let selectedValue;
-            for (const rb of rbs) {
-                if (rb.checked) {
-                    selectedValue = rb.value;
-                    break;
-                }
+    for (const option of options) {
+         if (option.checked) {
+             selectedValue = option.value;
+             break;
             }
+        }
     console.log(selectedValue);
 
-    //to check if answer is correct or not
+    //to check if the selected option is correct or not
     {
       selectedValue == questions[currentQuestionIndex].correctAnswer &&
       setScore(score+1)
@@ -79,7 +157,6 @@ function App() {
   let ele = document.getElementsByName("choice");
   for(var i=0; i<ele.length; i++)
       ele[i].checked = false;
-  
   }
 
   const startAgain = () => {
@@ -90,16 +167,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-          {currentQuestionIndex !== questions.length ?
+        
+          {
+          //If
+          currentQuestionIndex !== questions.length ?
             //fragment
             <>
+            
+            {/* <p>
+            <Progress completed={(currentQuestionIndex+1)/questions.length} />
+            </p> */}
+
             <p>
-              Question {currentQuestionIndex+1}/4
+              Question {currentQuestionIndex+1}/{questions.length}
             </p>
 
               <h1>
-                  {currentQuestionIndex+1}) {questions[currentQuestionIndex].Question}
+                  Q. {questions[currentQuestionIndex].Question}
               </h1>
 
               <p>
@@ -114,9 +198,8 @@ function App() {
 
               <button onClick={next}>NEXT</button>
             </>
-
+            //Else
             :
-
             <div>
             
             <h1>
@@ -126,7 +209,7 @@ function App() {
             <h2>
               {console.log(score)}
               {console.log(questions.length)}   
-              {score/4*100}% 
+              Score {score/(questions.length)*100}% 
             </h2>
             
             <p>
